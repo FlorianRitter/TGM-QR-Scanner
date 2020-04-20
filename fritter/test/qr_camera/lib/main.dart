df2d48b796972e822f8757dab2f7851f80e8fac0
Florian Ritter <ritter.florian@chello.at>
2020-04-20 22:42:51 +0200

    <fritter> QR-Scan und Player (kleiner Bug noch)

--- fritter/test/qr_camera/lib/main.dart
+++ fritter/test/qr_camera/lib/main.dart
@@ -1,10 +1,47 @@
 import 'package:flutter/material.dart';
 import 'package:permission_handler/permission_handler.dart';
+import 'package:qr_code_scanner/qr_code_scanner.dart';
+import 'package:qr_code_scanner/qr_scanner_overlay_shape.dart';
+import 'package:youtube_player_flutter/youtube_player_flutter.dart';
 
 void main() => runApp(MyApp());
 
+class YTRoute extends StatefulWidget {
+
+  final String link;
+
+  YTRoute({Key key, @required this.link}) : super(key: key);
+
+  @override
+  YTRouteState createState() => YTRouteState();
+}
+
+class YTRouteState extends State<YTRoute>{
+
+  YoutubePlayerController player;
+  YoutubePlayer view;
+
+  void loadPlayer(BuildContext context){
+    player = new YoutubePlayerController(initialVideoId: YoutubePlayer.convertUrlToId(widget.link));
+    player.fitHeight(MediaQuery.of(context).size);
+    view = YoutubePlayer(controller: player, progressColors: ProgressBarColors(playedColor: Colors.red, handleColor: Colors.redAccent));
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    loadPlayer(context);
+    return Scaffold(
+      body: Column(
+        children: <Widget>[
+        Expanded(
+          child: view
+        ),
+      ])
+    );
+  }
+}
+
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -27,17 +64,18 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-
+  var qrText = "";
+  QRViewController controller;
+  final GlobalKey qrKey = GlobalKey(debugLabel: 'QR');
   String status = "Press the button to determine the permission-Status";
 
   void askForPermission() async {
     String message = "";
-    if(await Permission.camera.request().isGranted){
+    if (await Permission.camera.request().isGranted) {
       message = "Camera working!";
       //TODO: Show camera
-    }
-    else{
-      message = "Camera not working!";  
+    } else {
+      message = "Camera not working!";
       //TODO: show no camera permission granted screen
     }
     setState(() {
@@ -45,27 +83,43 @@ class _MyHomePageState extends State<MyHomePage> {
     });
   }
 
+  void openYoutube(String l) {
+    MaterialPageRoute route = MaterialPageRoute(builder: (context) => YTRoute(link: l));
+    if(!route.isCurrent) {
+      Navigator.push(context, route);
+      controller.pauseCamera();
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              '$status',
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: askForPermission,
-        tooltip: 'Permission',
-        child: Icon(Icons.perm_camera_mic),
+      body: Column(
+        children: <Widget>[
+          Expanded(
+            flex: 5,
+            child: QRView(
+                key: qrKey,
+                overlay: QrScannerOverlayShape(
+                  borderRadius: 0,
+                  borderColor: Colors.green,
+                  borderLength: 30,
+                  borderWidth: 5,
+                  cutOutSize: 300),
+                onQRViewCreated: _onQRViewCreate),
+          )
+        ],
       ),
     );
+  }
+
+  void _onQRViewCreate(QRViewController controller) {
+    this.controller = controller;
+    controller.scannedDataStream.listen((onData) {
+      qrText = onData;
+      if(qrText != "") {
+        openYoutube(qrText);
+      }
+    });
   }
 }
